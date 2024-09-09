4bf7f7d669714cfc15791b21f4fa18d19f143614
Alexsander Antunes <alexsandercaac@gmail.com>
2024-09-09 21:48:24 +0000

    change: now filtering entire flights where there is a jump

--- src/cleaning.py
+++ src/cleaning.py
@@ -1,6 +1,7 @@
 """
-    Stage for filtering out the data that is not needed for the analysis.
+Stage for filtering out the data that is not needed for the analysis.
 """
+
 import os
 import datetime
 
@@ -12,55 +13,79 @@ from utils.dvc.params import get_params
 
 
 console = Console()
-params = get_params('cleaning')
+params = get_params("cleaning")
 
-if not os.path.exists(params['output_path']):
-    os.makedirs(params['output_path'])
+if not os.path.exists(params["output_path"]):
+    os.makedirs(params["output_path"])
 
-OUTPUT_PATH = params['output_path']
-INPUT_PATH = params['input_path']
-TIMEDELTA_THRESHOLD = params['timedelta_threshold']
+OUTPUT_PATH = params["output_path"]
+INPUT_PATH = params["input_path"]
+TIMEDELTA_THRESHOLD = params["timedelta_threshold"]
 
 TIMEDELTA_THRESHOLD = datetime.timedelta(minutes=TIMEDELTA_THRESHOLD)
 
 # List all files in the folder with the .parquet extension. Each file is
 # a daily partition of the dataset.
-PARQUET_FILES = [file for file in os.listdir(INPUT_PATH) if
-                 file.endswith('.parquet')]
+PARQUET_FILES = [
+    file for file in os.listdir(INPUT_PATH) if file.endswith(".parquet")
+]
 # Sort the files to ensure that the order is consistent.
 PARQUET_FILES.sort()
+n_files = len(PARQUET_FILES)
 
 console.log(f"Found {len(PARQUET_FILES)} files in {INPUT_PATH}")
 console.rule("Starting cleaning process...")
 
+# List files that are already present in the output folder.
+OUTPUT_FILES = [
+    file for file in os.listdir(OUTPUT_PATH) if file.endswith(".parquet")
+]
+
+# Only clean files that are not already present in the output folder.
+PARQUET_FILES = [file for file in PARQUET_FILES if file not in OUTPUT_FILES]
+
+previously_cleaned = n_files - len(PARQUET_FILES)
+
+if previously_cleaned > 0:
+    console.log(
+        f"Skipped {previously_cleaned} files that were already in the"
+        + "output folder."
+    )
+
 for file in track(PARQUET_FILES, description="Cleaning files..."):
     # Use lazy loading to avoid loading the entire dataset into memory.
-    daily_data = pl.scan_parquet(
-        os.path.join(INPUT_PATH, file)
-    )
+    daily_data = pl.scan_parquet(os.path.join(INPUT_PATH, file))
 
     # Create a column with the difference between the timestamp and the
     # timestamp at the previous row.
     daily_data = daily_data.with_columns(
-        timestamp_diff=pl.col('timestamp') - pl.col('timestamp').shift(1)
+        timestamp_diff=pl.col("timestamp") - pl.col("timestamp").shift(1)
     )
 
     # Filter out rows where the timestamp difference is greater than 10 minutes
     daily_data = daily_data.with_columns(
         pl.when(
-            (pl.col('timestamp_diff') > TIMEDELTA_THRESHOLD)
-            & (pl.col('flight_id') == pl.col('flight_id').shift(1)))
+            (pl.col("timestamp_diff") > TIMEDELTA_THRESHOLD)
+            & (pl.col("flight_id") == pl.col("flight_id").shift(1))
+        )
         .then(1)
-        .otherwise(0).alias("jump_indicator")
+        .otherwise(0)
+        .alias("jump_indicator")
     )
 
-    daily_data = daily_data.filter(pl.col('jump_indicator') == 0)
+    # Find the unique flight ids where the jump indicator is 1.
+    jump_ids = daily_data.filter(pl.col("jump_indicator") == 1).select(
+        "flight_id"
+    ).collect()
 
-    daily_data = daily_data.drop(['timestamp_diff', 'jump_indicator'])
+    # Filter out the rows where the flight id is in the list of jump ids.
+    daily_data = daily_data.filter(
+        pl.col("flight_id").is_in(jump_ids).not_()
+    )
+
+    daily_data = daily_data.drop(["timestamp_diff", "jump_indicator"])
 
     # Collect the data and write it to a new parquet file.
-    daily_data.collect().write_parquet(
-        os.path.join(OUTPUT_PATH, file)
-    )
+    daily_data.collect().write_parquet(os.path.join(OUTPUT_PATH, file))
 
 console.rule(f"Cleaned data saved to {OUTPUT_PATH}")
