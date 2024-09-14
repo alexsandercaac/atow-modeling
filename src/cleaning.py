5e0094e43903cf5286bafc10233891f7683a251f
Alexsander Antunes <alexsandercaac@gmail.com>
2024-09-13 21:36:36 -0300

    change: added counter for tracks analyzed and kept

--- src/cleaning.py
+++ src/cleaning.py
@@ -2,9 +2,15 @@
     Stage for filtering out the data that is not needed for the analysis.
 
     A set of parquet files, which are daily partitions of the dataset, are
-    loaded. The data is filtered to remove rows where the timestamp difference
-    between consecutive rows is greater than a given threshold, as these tracks
-    have incomplete data.
+    loaded. The folllowing filters of data quality are applied to the dataset:
+
+    * Row where the ground speed is equal to or less than zero are removed.
+
+    * Tracks that have a time difference between consecutive measurements
+        greater than a threshold are removed. This threshold is defined by the
+        TIMEDELTA_THRESHOLD parameter. Note that this is the last filter to be
+        applied, as the previous filters can generate missing data that would
+        be removed by this filter.
 
     Since the data is partitioned daily, the tracks can be split between two
     files. To ensure that the tracks are complete, the data from the following
@@ -80,6 +86,8 @@ else:
         + "output folder."
     )
 
+total_number_of_tracks = 0
+number_of_tracks_kept = 0
 for curr_file in track(
         range(len(PARQUET_FILES)), description="Cleaning files..."):
     # Both the current file and the following day's file are loaded.
@@ -98,9 +106,15 @@ for curr_file in track(
 
     flight_ids = daily_data.filter(
         pl.col("timestamp").dt.date() == curr_date
-    ).select("flight_id").collect()
+    ).select("flight_id").collect().unique()
+    total_number_of_tracks += len(flight_ids)
+    console.log(
+        f"Found {len(flight_ids)} tracks for {curr_date}."
+    )
 
     daily_data = daily_data.filter(pl.col("flight_id").is_in(flight_ids))
+
+    daily_data = daily_data.filter(pl.col("groundspeed") > 0)
 
     daily_data = daily_data.with_columns(
         timestamp_diff=pl.col("timestamp") - pl.col("timestamp").shift(1)
@@ -126,7 +140,13 @@ for curr_file in track(
 
     daily_data = daily_data.drop(["timestamp_diff", "jump_indicator"])
 
+    number_of_tracks_kept += daily_data.select(
+        "flight_id").collect().unique().shape[0]
+    console.log(f"Keeping {number_of_tracks_kept} tracks so far.")
+
     daily_data.collect().write_parquet(
         os.path.join(OUTPUT_PATH, PARQUET_FILES[curr_file]))
 
 console.rule(f"Cleaned data saved to {OUTPUT_PATH}")
+console.log(f"Total number of tracks processed: {total_number_of_tracks}")
+console.log(f"Number of tracks kept: {number_of_tracks_kept}")
